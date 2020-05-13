d72b1e4ef578c57233b8bc3c4c478d76c7e0402d
aagraw2 <amitagrawal235@gmail.com>
2020-05-13 20:24:27 +0530

    Added defailt routes in client, env variables in server

--- client/App.js
+++ client/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import { hot } from "react-hot-loader";
 import "./App.css";
+import Home from "Views/Home";
 
 class App extends Component {
     render() {
         return (
             <div className="App">
-                <h1> REACT TEMPLATE</h1>
+                <Home />
             </div>
         );
     }
---
+++ client/src/views/Home.js
@@ -0,0 +1,10 @@
+/* eslint-disable indent */
+import React from 'react';
+
+export default function Home() {
+    return (
+        <div>
+            <h1>HOME PAGE</h1>
+        </div>
+    );
+}
